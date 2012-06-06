203cf026633732d31245640eb2c398ba9d0341d5
AR Dahal <dahalaishraj@gmail.com>
2012-06-06 13:43:48 +0530

    Added the testing code to check the correctly parsed headers.

--- showHttp/showHttp.cpp
+++ showHttp/showHttp.cpp
@@ -24,7 +24,38 @@ void showHttp::slotResult(KJob* kjob)
 {
   KIO::Job* job = qobject_cast<KIO::Job*>(kjob);
   const QString httpHeaders = job ? job->queryMetaData("HTTP-Headers") : QString();
-  qDebug() << "HTTP HEADERS:" << httpHeaders;
+  //qDebug() << "HTTP HEADERS:" << httpHeaders;
+  parseHeaders(httpHeaders);
+}
+
+void showHttp::parseHeaders(const QString& httpHeader)
+{
+  QString trimedHeader = httpHeader.mid(httpHeader.indexOf('\n') + 1).trimmed();
+  
+  foreach (QString line, trimedHeader.split('\n')) {
+    int colon = line.indexOf(':');
+    QString headerName = line.left(colon).trimmed();
+    QString headerValue = line.mid(colon + 1).trimmed();  
+    int lessthan_pos = line.indexOf('<');
+    if (lessthan_pos >= 0) {
+      headerValue = line.mid(lessthan_pos + 1).trimmed();
+    }
+    m_headerInfo.insertMulti(headerName,headerValue);
+  }
+  
+  //Testing code
+  
+  QList<QString> mirror_values = m_headerInfo.values("link");
+  QList<QString> digest_values = m_headerInfo.values("digest");
+  qDebug() << "The miror values are";
+  for ( int i = 0; i < mirror_values.size(); i++) {
+    qDebug() << mirror_values.at(i);
+  }
+  qDebug() << "The hashes are" ;
+  foreach ( QString digest_val, digest_values) {
+    qDebug() << digest_val ;
+  }
+  exit(0);
 }
 
 #include "showHttp.moc"
