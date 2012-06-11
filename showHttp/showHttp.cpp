7f4fd3b9aceb358dd5f2b31e0f8add4dc04fe4a3
AR Dahal <dahalaishraj@gmail.com>
2012-06-11 21:08:36 +0530

    Removed the removal of less than sign removal code

--- showHttp/showHttp.cpp
+++ showHttp/showHttp.cpp
@@ -53,10 +53,6 @@ void showHttp::parseHeaders(const QString& httpHeader)
     int colon = line.indexOf(':');
     QString headerName = line.left(colon).trimmed();
     QString headerValue = line.mid(colon + 1).trimmed();  
-    int lessthan_pos = line.indexOf('<');
-    if (lessthan_pos >= 0) {
-      headerValue = line.mid(lessthan_pos + 1).trimmed();
-    }
     m_headerInfo.insertMulti(headerName,headerValue);
   }
   
